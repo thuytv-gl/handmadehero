be28d7c7ee5780f5757a62058920a0c0f9413569
thuy tran van <thuytranvan.gl@gmail.com>
2023-06-06 19:15:42 +0700

    init handmade

---
+++ handmade.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+
+// CONFIGS
+#define WINDOW_TITLE     "Handmade"
+#define WINDOW_WIDTH     1080
+#define WINDOW_HEIGHT    720
+#define FRAME_RATE       1000/60
+
+#define local_persist static
+#define global_variable static
