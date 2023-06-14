973ae9b2b5bb9b8226bdbc0bdb6ee10114007d26
thuy tran van <thuytranvan.gl@gmail.com>
2023-06-15 00:16:39 +0700

    day 01 + 02

--- misc/build.bat
+++ misc/build.bat
@@ -4,6 +4,6 @@ if not exist .\build mkdir .\build
 
 pushd .\build
 
-cl /Zi ..\handmade.cpp /link user32.lib
+cl /Zi ..\handmade.cpp /link user32.lib gdi32.lib
 
 popd 
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,6 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+w:
+cd handmadehero
 set path=w:\handmadehero\misc;%path%
 start alacritty
